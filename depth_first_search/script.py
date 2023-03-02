3df7fdcd8752b78e9d8944acce2f3a6debc34b3b
tambatra <tambatra00000000@gmail.com>
2023-03-02 10:55:25 +0100

    Writing unit test

--- depth_first_search/script.py
+++ depth_first_search/script.py
@@ -8,3 +8,4 @@ def depth_first_search(graph, start_vertex):
             for i in range(len(graph)):
                 if graph[vertex][i] != 0 and i not in visited:
                     queue.append(i)
+    return visited
