70e3128613b8d1b0c05063b94174acee44103419
tambatra <tambatra00000000@gmail.com>
2023-03-02 10:21:32 +0100

    Setting up the main program

---
+++ depth_first_search/depth_first_search.py
@@ -0,0 +1,10 @@
+def depth_first_search(graph, start_vertex):
+    visited = []
+    queue = [start_vertex]
+    while queue:
+        vertex = queue.pop()
+        if vertex not in visited:
+            visited.append(vertex)
+            for i in range(len(graph)):
+                if graph[vertex][i] != 0 and i not in visited:
+                    queue.append(i)
